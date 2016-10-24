72659bdce1629079bbe0c8324003cef95bf6d77f
Adam Stankiewicz <sheerun@sher.pl>
2016-10-24 02:11:18 +0200

    Use babel-runtime from peer dependency

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 async function hello(name) {
   return await new Promise(resolve => {
+    const words = { hello: 'hello' }
+    const { hello, ...rest } = { world: 'world', ...words }
     setTimeout(() => {
       resolve(`hello ${name}`)
     }, 500)
