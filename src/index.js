bcfb2635a71d109275421beb953dbf541b9bdd1d
Adam Stankiewicz <sheerun@sher.pl>
2017-04-19 15:30:38 +0200

    Update prettier-standard to 4.1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 async function hello (props) {
   return await new Promise(resolve => {
     const {name, ...rest} = {name: 'me', ...props}
-    setTimeout(
-      () => {
-        resolve(`hello ${name}`)
-      },
-      rest.length * 100
-    )
+    setTimeout(() => {
+      resolve(`hello ${name}`)
+    }, rest.length * 100)
   })
 }
 
