f6319f251752884e4fef9b1b7c20e2ae3d82a3ed
Adam Stankiewicz <sheerun@sher.pl>
2019-09-20 23:27:01 +0200

    Try to fix tests

--- test/cli.test.js
+++ test/cli.test.js
@@ -32,7 +32,9 @@ it('works for js repository', () => {
   execSync('yarn lint', { cwd: 'test/sandbox' })
   execSync('echo "console.log(   12);" > index.js', { cwd: 'test/sandbox' })
   execSync('git add -A', { cwd: 'test/sandbox' })
-  execSync('git commit -m test', { cwd: 'test/sandbox' })
+  execSync('git commit -m test --author "User <user@example.com>"', {
+    cwd: 'test/sandbox'
+  })
 
   const contents = fs.readFileSync('test/sandbox/index.js', 'utf-8')
   expect(contents).toEqual('console.log(12)' + os.EOL)
